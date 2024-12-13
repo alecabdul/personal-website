fadfed731c9ac86632097a1e9550c6cc1d0b9996
alecabdul <alecabdul99@gmail.com>
2024-12-12 20:09:11 -0800

    Update fonts.ts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,4 +2,4 @@ import { Inter, Lusitana, Roboto} from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ["latin"]})  
-export const roboto = Roboto({ weight: ["100", "300", "400", "500", "700", "900"]})  
+export const roboto = Roboto({ weight: ["100", "300", "400", "500", "700", "900"], subsets: ['latin'] })
