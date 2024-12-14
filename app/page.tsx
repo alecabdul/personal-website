6f9c654111ef341ad4683cba90a1be545551e4a0
alecabdul <alecabdul99@gmail.com>
2024-12-13 19:33:38 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,11 @@ import { DocumentTextIcon, AtSymbolIcon } from '@heroicons/react/24/outline';
 
 export default function Page() {
   return (
-    <main className="flex bg-white-100 text-gray-600 flex-col py-6 p-10">
-      {/* Intro Section */}
-      <section>
-        {/* <div className='relative gap-6'>
+    <main>
+      <div className="flex bg-white-100 text-gray-600 flex-col py-6 p-10">
+        {/* Intro Section */}
+        <section id="Intro">
+          {/* <div className='relative gap-6'>
           <Link
             href="/Alec_Abdul-Rahim_Resume.pdf"
             target="_blank"
@@ -21,16 +22,16 @@ export default function Page() {
             <span >Résumé</span> <DocumentTextIcon className='w-7 h-7' />
           </Link>
         </div> */}
-        <div className="flex flex-col gap-4 min-h-screen md:flex-row">
-          <div className="flex rounded-xl flex-col justify-center gap-4 bg-gray-50 px-6 py-10 md:w-2/5 md:px-12">
-            <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-              <strong>Hi, I'm Alec.</strong></p>
-            <p>I'm a <strong>Software Quality Engineer</strong> currently based in Lake Tahoe. My goal is to keep growing my technical knowledge while collaborating with teams to deliver solutions that people love.
-            </p>
-            <p> When I'm not behind the keyboard, you'll find me hiking, snowboarding, trying new food, or exploring new places.</p>
-            <p> This website is built in effort to improve my <strong>React</strong> and <strong>Next.js</strong> skills, so it is a WIP.
-            </p>
-            {/* <p> Feel free to contact or follow me:
+          <div className="flex flex-col gap-4 min-h-screen md:flex-row">
+            <div className="flex rounded-xl flex-col justify-center gap-4 bg-gray-50 px-6 py-10 md:w-1/2 md:px-24">
+              <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+                <strong>Hi, I'm Alec.</strong></p>
+              <p>I'm a <strong>Software Quality Engineer</strong> currently based in Lake Tahoe. My goal is to keep growing my technical knowledge while collaborating with teams to deliver solutions that people love.
+              </p>
+              <p> When I'm not behind the keyboard, you'll find me hiking, snowboarding, trying new food, or exploring new places.</p>
+              <p> This website is built in effort to improve my <strong>React</strong> and <strong>Next.js</strong> skills, so it is a WIP.
+              </p>
+              {/* <p> Feel free to contact or follow me:
             </p>  
             <span className='flex flex-row gap-4'>
               <Link
@@ -67,187 +68,185 @@ export default function Page() {
                 />
               </Link>
             </span> */}
+            </div>
+            <div className="flex flex-col items-center justify-center rounded-xl bg-gray-50 md:w-1/2 md:h-auto">
+              <div
+                className="relativeflex items-center justify-center"
+                style={{
+                  padding: '24px', // Padding between the image and the gray background
+                }}
+              >
+                <Image
+                  src="/riflepeak.jpeg"
+                  layout="intrinsic"
+                  width={700}
+                  height={525}
+                  alt="Screenshots of the dashboard project showing desktop version"
+                  className="rounded-lg"
+                />
+              </div>
+            </div>
           </div>
-          <div className="flex flex-col items-center justify-center rounded-xl bg-gray-50 md:w-3/5 md:h-auto">
-            <div
-              className="relativeflex items-center justify-center"
-              style={{
-                padding: '12px', // Padding between the image and the gray background
-              }}
-            >
+        </section>
+
+        {/* New Experience Section */}
+        <section  id="Experience" className="mt-10 bg-gray-50 p-10 rounded-xl">
+          <h2 className="text-2xl font-bold text-gray-800 md:text-3xl">Experience</h2>
+          <p className="mt-4 text-xl leading-relaxed">
+            In my career so far, I've worn many hats and taken a lot of ownership, which has given me great opportunity to develop a variety of skills - project management, technical, and leadership skills.</p>
+
+          {/* RL Desc. */}
+          <div className="mt-10 text-gray-600 md:text-xl leading-relaxed">
+            <span className='flex flex-row gap-1'>
+              <strong>Ridgeline Apps
+              </strong>
               <Image
-                src="/riflepeak.jpeg"
-                layout="intrinsic"
-                width={800}
-                height={600}
-                alt="Screenshots of the dashboard project showing desktop version"
-                className="rounded-lg"
+                src="/Ridgeline.png"
+                width={30}
+                height={30}
+                style={{ objectFit: "contain" }}
+                alt="LinkedIn Icon"
               />
-            </div>
+            </span>
+            <p>
+              Vertically integrated financial software for asset management firms.
+            </p>
           </div>
-        </div>
-      </section>
 
-      {/* New Experience Section */}
-      <section className="mt-10 bg-gray-50 p-10 rounded-xl">
-        <h2 className="text-2xl font-bold text-gray-800 md:text-3xl">Experience</h2>
-        <p className="mt-4 text-xl leading-relaxed">
-          In my career so far, I've worn many hats and taken a lot of ownership, which has given me great opportunity to develop a variety of skills - project management, technical, and leadership skills.</p>
-
-        {/* RL Desc. */}
-        <div className="mt-10 text-gray-600 md:text-xl leading-relaxed">
-          <span className='flex flex-row gap-1'>
-            <strong>Ridgeline Apps
-            </strong>
-            <Image
-              src="/Ridgeline.png"
-              width={30}
-              height={30}
-              style={{ objectFit: "contain" }}
-              alt="LinkedIn Icon"
-            />
-          </span>
-          <p>
-            Vertically integrated financial software for asset management firms.
+          {/* QEII */}
+          <p className='px-4 md:text-lg  leading-relaxed mt-6'>
+            <strong>Quality Engineer II</strong>, February 2024 - Present
           </p>
-        </div>
-
-        {/* QEII */}
-        <p className='px-4 md:text-lg  leading-relaxed mt-6'>
-          <strong>Quality Engineer II</strong>, February 2024 - Present
-        </p>
-        <p className='leading-relaxed px-4'>
-          Expanded responsibilities to be sole QE for the newly formed Client Portal team, introducing new challenges for Ridgeline where maintaining performance quality was mission-critical. I owned the entire quality assurance process, from product ideation to go-live, and continue to ensure its success today.
-        </p>
-
-        {/* QEI */}
-        <p className='px-4 md:text-lg  leading-relaxed mt-6'>
-          <strong>Quality Engineer I</strong>, February 2023 - February 2024
-        </p>
-        <p className='leading-relaxed px-4'>
-          Joined full-time at Ridgeline as the sole QE supporting the Revenue Management team. Developed comprehensive test automation suites, executed test plans, and managed code deployments.
-        </p>
-
-        {/* QA Intern */}
-        <p className='px-4 md:text-lg leading-relaxed mt-6'>
-          <strong>QA Engineer Intern</strong>, June 2022 - August 2022
-        </p>
-        <p className='leading-relaxed px-4 text'>
-          Created manual test plans, developed UI and API test automation, and collaborated with Product Managers and Software Engineers to deliver top-notch features in an AGILE fashion.
-        </p>
-
-        {/* HP  Desc. */}
-        <div className="mt-12 text-gray-600 md:text-xl leading-relaxed">
-          <span className='flex flex-row gap-2'>
-            <strong>HP Inc
-            </strong>
-            <Image
-              src="/hp.png"
-              width={30}
-              height={30}
-              style={{ objectFit: "contain" }}
-              alt="HP Icon"
-            />
-          </span>
-          <p>
-            Tech giant renowned for its laptops, desktops, and printers.
+          <p className='leading-relaxed px-4'>
+            Expanded responsibilities to be sole QE for the newly formed Client Portal team, introducing new challenges for Ridgeline where maintaining performance quality was mission-critical. I owned the entire quality assurance process, from product ideation to go-live, and continue to ensure its success today.
           </p>
-        </div>
 
-        {/* Data Analyst Intern */}
-        <p className='px-4 md:text-lg leading-relaxed mt-6'>
-          <strong>Data Analyst Intern</strong>, June 2020 - August 2021
-        </p>
-        <p className='leading-relaxed px-4'>
-          Dove into large datasets and applied data-driven insights to improve HP's Amazon listing strategy for the Omnichannel Strategy team. The internship was extended through the school year and into August 2021.
-        </p>
-      </section>
+          {/* QEI */}
+          <p className='px-4 md:text-lg  leading-relaxed mt-6'>
+            <strong>Quality Engineer I</strong>, February 2023 - February 2024
+          </p>
+          <p className='leading-relaxed px-4'>
+            Joined full-time at Ridgeline as the sole QE supporting the Revenue Management team. Developed comprehensive test automation suites, executed test plans, and managed code deployments.
+          </p>
 
-      {/* New Education Section */}
-      <section className="mt-10 bg-gray-50 p-10 rounded-xl">
-        <h2 className="text-2xl font-bold text-gray-800 md:text-3xl">Education</h2>
-        <p className="mt-4 text-gray-600 text-xl leading-relaxed">
-          <span className='flex flex-row gap-2'>
-            <strong> University of California, Los Angeles
-            </strong>
-            <Image
-              src="/ucla.png"
-              width={40}
-              height={40}
-              style={{ objectFit: "contain" }}
-              alt="UCLA Icon"
-            />
-          </span>
-        </p>
-        <p className='text-lg'>
-          B.A. Economics, Minor in Statistics
-        </p>
-        <p className='text-lg'>
-          September 2018 - December 2022
-        </p>
-        <p className='list-disc mt-4 leading-relaxed px-4'>
-          Attended the <strong>#1</strong> public university in the US, where I pursed a diverse academic curriculum through theoretical and mathematical economics, statistics, and a variety of liberal arts courses. Also studied abroad in Florence in the Fall of 2021.       </p>
-      </section>
+          {/* QA Intern */}
+          <p className='px-4 md:text-lg leading-relaxed mt-6'>
+            <strong>QA Engineer Intern</strong>, June 2022 - August 2022
+          </p>
+          <p className='leading-relaxed px-4 text'>
+            Created manual test plans, developed UI and API test automation, and collaborated with Product Managers and Software Engineers to deliver top-notch features in an AGILE fashion.
+          </p>
 
-
-      <section className="mt-10 bg-gray-50 p-10 rounded-xl">
-        <h2 className="text-2xl font-bold text-gray-800 md:text-3xl">Projects</h2>
-
-        <div className="flex flex-col md:flex-row justify-center py-4 gap-4">
-
-          {/* Personal Website */}
-          <div className="bg-gray-200 rounded-xl p-4 max-w-md w-full flex flex-col justify-center items-center">
-
-            <p className="px-4 md:text-lg leading-relaxed text-center">
-              <strong>Personal Website</strong>
+          {/* HP  Desc. */}
+          <div className="mt-12 text-gray-600 md:text-xl leading-relaxed">
+            <span className='flex flex-row gap-2'>
+              <strong>HP Inc
+              </strong>
+              <Image
+                src="/hp.png"
+                width={30}
+                height={30}
+                style={{ objectFit: "contain" }}
+                alt="HP Icon"
+              />
+            </span>
+            <p>
+              Tech giant renowned for its laptops, desktops, and printers.
             </p>
-            <p className="leading-relaxed px-4 text text-center">
-              This website you're on is a way for me to tell more about myself beyond the Resume and have a glimpse into who I am on a more personal level.
-            </p>
-            <div>
-              <ul className='flex flex-row flex-wrap justify-center items-center gap-2 mt-2'>
-                <li className='bg-gray-300  rounded-xl px-2 py-1'>TypeScript</li>
-                <li className='bg-gray-300  rounded-xl px-2 py-1'>React</li>
-                <li className='bg-gray-300  rounded-xl px-2 py-1'>Next.js</li>
-                <li className='bg-gray-300  rounded-xl px-2 py-1'>Tailwind</li>
-                <li className='bg-gray-300  rounded-xl px-2 py-1'>Vercel</li>
-              </ul>
+          </div>
+
+          {/* Data Analyst Intern */}
+          <p className='px-4 md:text-lg leading-relaxed mt-6'>
+            <strong>Data Analyst Intern</strong>, June 2020 - August 2021
+          </p>
+          <p className='leading-relaxed px-4'>
+            Dove into large datasets and applied data-driven insights to improve HP's Amazon listing strategy for the Omnichannel Strategy team. The internship was extended through the school year and into August 2021.
+          </p>
+        </section>
+
+        {/* New Education Section */}
+        <section id="Education" className="mt-10 bg-gray-50 p-10 rounded-xl">
+          <h2 className="text-2xl font-bold text-gray-800 md:text-3xl">Education</h2>
+          <p className="mt-4 text-gray-600 text-xl leading-relaxed">
+            <span className='flex flex-row gap-2'>
+              <strong> University of California, Los Angeles
+              </strong>
+              <Image
+                src="/ucla.png"
+                width={40}
+                height={40}
+                style={{ objectFit: "contain" }}
+                alt="UCLA Icon"
+              />
+            </span>
+          </p>
+          <p className='text-lg'>
+            B.A. Economics, Minor in Statistics
+          </p>
+          <p className='text-lg'>
+            September 2018 - December 2022
+          </p>
+          <p className='list-disc mt-4 leading-relaxed px-4'>
+            Attended the <strong>#1</strong> public university in the US, where I pursed a diverse academic curriculum through theoretical and mathematical economics, statistics, and a variety of liberal arts courses. Also studied abroad in Florence in the Fall of 2021.       </p>
+        </section>
+
+
+        <section id="Projects" className="mt-10 bg-gray-50 p-10 rounded-xl">
+          <h2 className="text-2xl font-bold text-gray-800 md:text-3xl">Projects</h2>
+
+          <div className="flex flex-col md:flex-row justify-center py-4 gap-4">
+
+            {/* Personal Website */}
+            <div className="bg-gray-200 rounded-xl p-4 max-w-md w-full flex flex-col justify-center items-center">
+
+              <p className="px-4 md:text-lg leading-relaxed text-center">
+                <strong>Personal Website</strong>
+              </p>
+              <p className="leading-relaxed px-4 text text-center">
+                This website you're on is a way for me to tell more about myself beyond the Resume and have a glimpse into who I am on a more personal level.
+              </p>
+              <div>
+                <ul className='flex flex-row flex-wrap justify-center items-center gap-2 mt-2'>
+                  <li className='bg-gray-300  rounded-xl px-2 py-1'>TypeScript</li>
+                  <li className='bg-gray-300  rounded-xl px-2 py-1'>React</li>
+                  <li className='bg-gray-300  rounded-xl px-2 py-1'>Next.js</li>
+                  <li className='bg-gray-300  rounded-xl px-2 py-1'>Tailwind</li>
+                  <li className='bg-gray-300  rounded-xl px-2 py-1'>Vercel</li>
+                </ul>
+              </div>
             </div>
-          </div>
 
-          {/* Replit Agent */}
-          <div className="bg-gray-200 rounded-xl p-4 max-w-md w-full flex flex-col justify-center items-center">
-            <p className="px-4 md:text-lg leading-relaxed text-center">
-              <strong>Bay Area Sports Trivia</strong>
-            </p>
-            <p className="leading-relaxed px-4 text text-center">
-              A game testing the user's knowledge of Bay Area sports. It is multiple choice and tracks the user's high score.
-            </p>
-            <div>
-              <ul className='flex flex-row flex-wrap justify-center items-center gap-2 mt-2'>
-                <li className='bg-gray-300  rounded-xl px-2 py-1'>Replit Agent</li>
-                <li className='bg-gray-300  rounded-xl px-2 py-1'>JavaScript</li>
-                <li className='bg-gray-300  rounded-xl px-2 py-1'>CSS</li>
-                <li className='bg-gray-300  rounded-xl px-2 py-1'>Python</li>
-                <li className='bg-gray-300  rounded-xl px-2 py-1'>SQL</li>
-              </ul>
+            {/* Replit Agent */}
+            <div className="bg-gray-200 rounded-xl p-4 max-w-md w-full flex flex-col justify-center items-center">
+              <p className="px-4 md:text-lg leading-relaxed text-center">
+                <strong>Bay Area Sports Trivia</strong>
+              </p>
+              <p className="leading-relaxed px-4 text text-center">
+                A game testing the user's knowledge of Bay Area sports. It is multiple choice and tracks the user's high score.
+              </p>
+              <div>
+                <ul className='flex flex-row flex-wrap justify-center items-center gap-2 mt-2'>
+                  <li className='bg-gray-300  rounded-xl px-2 py-1'>Replit Agent</li>
+                  <li className='bg-gray-300  rounded-xl px-2 py-1'>JavaScript</li>
+                  <li className='bg-gray-300  rounded-xl px-2 py-1'>Python</li>
+                  <li className='bg-gray-300  rounded-xl px-2 py-1'>SQL</li>
+                </ul>
+              </div>
             </div>
+
+            {/* Maps */}
+            <div className="bg-gray-200 rounded-xl p-4 max-w-md w-full flex flex-col justify-center items-center">
+              <p className="px-4 md:text-lg leading-relaxed text-center">
+                <strong>Favorites Map Locator</strong>
+              </p>
+              <p className="leading-relaxed px-4 text text-center">
+                My favorite places to eat, drink, or hangout using the Google Maps platform. Coming soon...
+              </p>
+            </div>
+
           </div>
-
-          {/* Maps */}
-          <div className="bg-gray-200 rounded-xl p-4 max-w-md w-full flex flex-col justify-center items-center">
-            <p className="px-4 md:text-lg leading-relaxed text-center">
-              <strong>Favorites Map Locator</strong>
-            </p>
-            <p className="leading-relaxed px-4 text text-center">
-              My favorite places to eat, drink, or hangout using the Google Maps platform. Coming soon...
-            </p>
-          </div>
-
-        </div>
-
-
-      </section>
+        </section>
+      </div>
     </main>
   );
 }
