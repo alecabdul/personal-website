e93723e4aa364a6f168ddcac70f68be669ff3427
alecabdul <alecabdul99@gmail.com>
2024-12-14 20:46:17 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -244,7 +244,7 @@ export default function Page() {
             <li>I'm a native Spanish speaker</li>
             <li>I've traveled to over 20 countries</li>
             <li>I'm an Eagle Scout and am prepared for anything</li>
-            <li>I once lived in Florence, where I became enthralled by its history and pistachio gelato</li>
+            <li>I once lived in Florence, where I was captivated by its history and pistachio gelato</li>
             <li>I love attending music festivals and have been to some of the biggest</li>
           </ul>
         </section>
