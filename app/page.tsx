635158da14953823da27dca4a86b4c4b27c03634
alecabdul <alecabdul99@gmail.com>
2024-12-29 09:42:13 -0800

    push

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function Page() {
         <section id="Background" className="mt-10 bg-gray-50 p-10 rounded-xl">
           <h2 className="text-2xl font-bold text-gray-800 md:text-3xl">Background</h2>
           <p className="mt-2 text-lg leading-relaxed">
-            Working at a fast-paced startup, I've taken ownership and delivered results to be proud of. The pressure is always on, but I thrive in this environment.
+            Working at a fast-paced startup, I've worn many hats and taken great ownership in order to deliver stellar results. This has meant taking on responsibilities beyond the scope of a typical QA Engineer to make sure the best solutions are delivered.
           </p>
 
           {/* RL Desc. */}
@@ -69,23 +69,21 @@ export default function Page() {
             <strong>Quality Engineer II</strong>, February 2024 - Present
           </p>
           <p className='leading-relaxed px-4'>
-            Expanded responsibilities to be sole QE for the newly formed Client Portal team, owning the all quality assurance processes. I introduced a new client-side load testing framework that helped numerous teams across the company identify and act on performance bottlenecks.
-          </p>
+          Expanded my responsibilities to become the sole QE for the newly formed Client Portal team, working cross-functionally and owning all quality assurance processes. I introduced a new UI load testing framework to the company that helped multiple teams identify and resolve performance bottlenecks. Additionally, I served as the resident demo voice for Client Portal, showcasing our new product to numerous prospective customers.          </p>
 
           {/* QEI */}
           <p className='px-4 text-lg  leading-relaxed mt-6'>
             <strong>Quality Engineer I</strong>, February 2023 - February 2024
           </p>
           <p className='leading-relaxed px-4'>
-            Joined full-time at Ridgeline as the sole QE supporting the Revenue Management team. I developed comprehensive test automation suites, owned test plans, and managed code deployments.
-          </p>
+          Joined Ridgeline full-time as the sole QE supporting the Revenue Management team, where I owned all test plans and developed comprehensive test automation suites. I quickly became the team's go-to product expert, and used this knowledge to lead efforts in troubleshooting customer issues. I also managed service deployments and feature flag flips, using my expertise to build a deployment framework that was later adopted by other teams.          </p>
 
           {/* QA Intern */}
           <p className='px-4 text-lg leading-relaxed mt-6'>
             <strong>QA Engineer Intern</strong>, June 2022 - August 2022
           </p>
           <p className='leading-relaxed px-4 text'>
-            Created manual test plans, developed UI and API test automation, and collaborated with Product Managers and Software Engineers to deliver top-notch features using Agile methods.
+            Created manual test plans, developed UI and API test automation, and collaborated with Product Managers and Software Engineers to deliver top-notch features using Agile methods. I was also given the intern award for "Most Majestic Beard", but have since shaved.
           </p>
 
           {/* HP  Desc. */}
@@ -134,7 +132,7 @@ export default function Page() {
             <strong>B.A. Economics, Minor in Statistics</strong>, September 2018 - December 2022
           </p>
           <p className='leading-relaxed px-4'>
-            Tackled a diverse curriculum of theoretical and mathematical economics, statistics, and a variety of liberal arts courses. Go Bruins!
+          Tackled a diverse curriculum in theoretical and mathematical economics, statistics, and a variety of liberal arts courses. I also made the most of UCLA's top ranking in on-campus food across all universities. Go Bruins!
           </p>
         </section>
 
